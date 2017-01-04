cda106dd3a137668d9aa3a9f356b7665202e75cd
thanhproz2 <ducthanh448@gmail.com>
2017-01-04 16:47:09 +0700

    complete #5: Create category for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,33 +2,43 @@ class ArticlesController < ApplicationController
 before_action :authenticate_user! ,  except: [:index, :show]
 http_basic_authenticate_with name: "thanh", password: "222222", except: [:index, :show]  
 
-def index
-    @articles = Article.all
+  def index
+    if params[:category].blank?
+      @articles = Article.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles =Article.where(:category_id => @category_id).order("created_at DESC")
+    end
+      
+  end  
+    #@articles = Article.all
    # @u =User.all
     #@first_name = current_user.first_name
     #@a="asdasd"
      # => @last_name = current_user.last_name
- end
+ 
  
   def show
     @article = Article.find(params[:id])
   end
  
   def new
-
+    
     @article = current_user.articles.new
     #a = current_user.id
-    
+    @categories=Category.all.map{ |c| [c.name, c.id] }
   end
  
   def edit
     @article = Article.find(params[:id])
+    @categories=Category.all.map{ |c| [c.name, c.id] }
   end
  
   def create
     
     @article = current_user.articles.new(article_params)
- 
+    @categories=Category.all.map{ |c| [c.name, c.id] }
+    @article.category_id = params[:category_id]
     if @article.save
       redirect_to @article
     else
@@ -56,6 +66,6 @@ def index
 
   private
     def article_params
-      params.require(:article).permit(:title, :text)
+      params.require(:article).permit(:title, :text, :category_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220083747) do
+ActiveRecord::Schema.define(version: 20161230061041) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
