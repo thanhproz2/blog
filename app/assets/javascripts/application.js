5c7271ed81c862a4909bd11eab3a7e6250017f7b
thanhproz2 <ducthanh448@gmail.com>
2017-01-05 16:35:01 +0700

    complete issue #5 Create category for article

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require ckeditor/init
 
 //= require_tree .
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,5 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
-  has_attached_file :article_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :article_img, content_type: /\Aimage\/.*\z/
+
 end
